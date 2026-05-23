8aea4e961da423b7813431497fe59d05b928fe6b
Ezz10099 <playpassezz@gmail.com>
2026-05-23 15:14:42 +0300

    Rename preload title to Darkness Kingdom

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -15,7 +15,7 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(240, 400, 'ARCANE ACADEMY', { font: '28px monospace', fill: '#ffd700' }).setOrigin(0.5);
+    this.add.text(240, 400, 'DARKNESS KINGDOM', { font: '28px monospace', fill: '#ffd700' }).setOrigin(0.5);
     this.add.text(240, 450, 'Loading...', { font: '18px monospace', fill: '#aaaaaa' }).setOrigin(0.5);
     this.time.delayedCall(800, () => {
       this.cameras.main.fadeOut(400, 0, 0, 0);
